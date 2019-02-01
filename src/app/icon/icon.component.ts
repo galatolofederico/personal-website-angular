d4ccc3fe7868d750fce84fe7f9b28a77c0836880
Federico Galatolo <galatolo.federico@gmail.com>
2019-02-01 20:55:41 +0100

    Minor adds

--- src/app/icon/icon.component.ts
+++ src/app/icon/icon.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {NgbPopoverConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss']
+  styleUrls: ['./icon.component.scss'],
+  providers: [NgbPopoverConfig] 
 })
 export class IconComponent implements OnInit {
-  @Input() icon;
-  constructor() { }
+  @Input() profile;
+  constructor(config: NgbPopoverConfig) {
+    config.placement = "bottom";
+    config.triggers = "hover"
+  }
 
   ngOnInit() {
   }
