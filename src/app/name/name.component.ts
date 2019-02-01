1257abbbfb98b6ef7d4a7dde46463e7c0fe08540
Federico Galatolo <galatolo.federico@gmail.com>
2019-02-01 14:31:26 +0100

    Integrated typed.js

--- src/app/name/name.component.ts
+++ src/app/name/name.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-name',
@@ -10,6 +11,14 @@ export class NameComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var typed = new Typed("#typed", {
+        strings: ["PhD Student", "Machine Learning Engineer", "Computer Engineer"],
+        showCursor: false,
+        typeSpeed: 60,
+        backSpeed: 30,
+        backDelay: 1000,
+        loop: true
+    });
   }
 
 }
