67836ec89a6331b576c923dc526b7be2eb8a07ac
Federico Galatolo <galatolo.federico@gmail.com>
2019-02-01 02:27:59 +0100

    Bootstraped css, created name component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NameComponent } from './name/name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule,
